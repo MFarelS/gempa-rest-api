9385390bb69141685ce1128d28d5b972808f4425
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-04 19:23:46 +0700

    Update and rename YT.js to Muslim.js

---
+++ utils/Muslim.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+async function Random() {
+    let c;
+	let url = [
+			'https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/wallpaper/Muslim.txt'			
+	]
+    let a = url[Math.floor(Math.random() * url.length)]
+    await fetch(a).then(res => res.text()).then(body => {
+            let b = body.split("\n")
+            c = b[Math.floor(Math.random() * b.length)]
+        });
+    return c;
+}
+
+const Muslim = () => new Promise((resolve, reject) => {
+    Random().then(url => {
+		resolve({
+			url
+		})
+	}).catch(err => { reject(err) })
+})
+
+module.exports = Muslim
