f9582bafe4cb084930ccd3c4a3a0abf19da181b5
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-05 21:37:54 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const router  = express.Router()
+const express = require("express");
+const app = express();
+const port = 8080;
+const cors = require("cors");
+const http = require("http");
+const https = require("https");
+const path = require("path")
+const Zahir = require('../utils')
 
-const {
-	Muslim
-} = require('../utils')
+http.globalAgent.maxSockets = Infinity;
+https.globalAgent.maxSockets = Infinity;
 
-router.get('/api/wallpaper/muslim', (req, res) => {
-	Muslim()
-			.then(url => {
-				res.send(url)
-			})
-			.catch(err => {
-				res.send(err);
-			})
-})
+app.use(cors());
 
-module.exports = router
+app.get("/", (req, res) => {
+  setImmediate(() => {
+    try {
+      res.setHeader("Cache-Control", "public,max-age=0");
+      res.sendFile(path.join(__dirname, "../index.html"));
+    } catch (e) {
+      res.status(400).send("Something went wrong");
+    }
+  });
+});
+
+app.get("/api/gempa", (req, res) => {
+  setImmediate(() => {
+    try {
+      res.setHeader("Cache-Control", "public,max-age=3600,s-maxage=30");
+      Zahir.Gempa()
+      .then((data) => {
+       res.json(data);
+      })
+    } catch (e) {
+	    res.status(400).send("Server Maintance");
+    }
+  });
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
